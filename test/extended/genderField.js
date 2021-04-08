5f215d746e8c68fe79cb520cb21915c67b2b02ab
shura <shurasanyatx@gmail.com>
2021-04-07 23:54:16 -0400

    Age Field TC 041-045

---
+++ test/extended/genderField.js
@@ -0,0 +1,100 @@
+import sel from '../../data/selectors';
+import {name, gender, age, story} from '../../data/testData';
+
+describe('Gender', function () {
+
+    before('Open App', function () {
+        browser.url('');
+    });
+
+    describe('Radio Buttons are enabled', function () {
+
+        it('TC-041.a Button "he" is enabled', function () {
+            $$(sel.radioButtons)[gender.he].click();
+            let radioButtonIsBlue = $$(sel.radioButtons)[gender.he].isEnabled();
+            expect(radioButtonIsBlue).toEqual(true);
+        });
+
+        it('TC-042 Button "she" is enabled', function () {
+            $$(sel.radioButtons)[gender.she].click();
+            let radioButtonIsBlue = $$(sel.radioButtons)[gender.she].isEnabled();
+            expect(radioButtonIsBlue).toEqual(true);
+        });
+
+        it('TC-043 Button "it" is enabled', function () {
+            $$(sel.radioButtons)[gender.it].click();
+            let radioButtonIsBlue = $$(sel.radioButtons)[gender.it].isEnabled();
+            expect(radioButtonIsBlue).toEqual(true);
+        });
+    });
+
+    describe('Button "he"', function () {
+
+        it('TC-044.a User can choose only one button at the time: he', function () {
+            $$(sel.radioButtons)[gender.she].$('input').click();
+            $$(sel.radioButtons)[gender.he].$('input').click();
+            let radioButtonIsGray = $$(sel.radioButtons)[gender.he].$('input').isSelected();
+            expect(radioButtonIsGray).toEqual(true);
+        });
+
+        it('TC-044.b "SHE" is not selected', function () {
+            $$(sel.radioButtons)[gender.she].$('input').click();
+            $$(sel.radioButtons)[gender.he].$('input').click();
+            let radioButtonIsGray = $$(sel.radioButtons)[gender.she].$('input').isSelected();
+            expect(radioButtonIsGray).toEqual(false);
+        });
+
+        it('TC-044.c "IT" is not selected', function () {
+            $$(sel.radioButtons)[gender.she].$('input').click();
+            $$(sel.radioButtons)[gender.he].$('input').click();
+            let radioButtonIsGray = $$(sel.radioButtons)[gender.it].$('input').isSelected();
+            expect(radioButtonIsGray).toEqual(false);
+        });
+    });
+
+    describe('Button "she"', function () {
+
+        it('TC-045.a User can choose only one button at the time: she', function () {
+            $$(sel.radioButtons)[gender.he].$('input').click();
+            $$(sel.radioButtons)[gender.she].$('input').click();
+            let radioButtonIsGray = $$(sel.radioButtons)[gender.she].$('input').isSelected();
+            expect(radioButtonIsGray).toEqual(true);
+        });
+
+        it('TC-045.b "HE" is not selected', function () {
+            $$(sel.radioButtons)[gender.he].$('input').click();
+            $$(sel.radioButtons)[gender.she].$('input').click();
+            let radioButtonIsGray = $$(sel.radioButtons)[gender.he].$('input').isSelected();
+            expect(radioButtonIsGray).toEqual(false);
+        });
+        it('TC-045.c "IT" is not selected', function () {
+            $$(sel.radioButtons)[gender.it].$('input').click();
+            $$(sel.radioButtons)[gender.he].$('input').click();
+            let radioButtonIsGray = $$(sel.radioButtons)[gender.it].$('input').isSelected();
+            expect(radioButtonIsGray).toEqual(false);
+        });
+    });
+
+    describe('Button "it"', function () {
+
+        it('TC-045.a User can choose only one button at the time: it', function () {
+            $$(sel.radioButtons)[gender.she].$('input').click();
+            $$(sel.radioButtons)[gender.it].$('input').click();
+            let radioButtonIsGray = $$(sel.radioButtons)[gender.it].$('input').isSelected();
+            expect(radioButtonIsGray).toEqual(true);
+        });
+
+        it('TC-045.b "HE" is not selected', function () {
+            $$(sel.radioButtons)[gender.he].$('input').click();
+            $$(sel.radioButtons)[gender.it].$('input').click();
+            let radioButtonIsGray = $$(sel.radioButtons)[gender.he].$('input').isSelected();
+            expect(radioButtonIsGray).toEqual(false);
+        });
+        it('TC-045.c "SHE" is not selected', function () {
+            $$(sel.radioButtons)[gender.she].$('input').click();
+            $$(sel.radioButtons)[gender.it].$('input').click();
+            let radioButtonIsGray = $$(sel.radioButtons)[gender.she].$('input').isSelected();
+            expect(radioButtonIsGray).toEqual(false);
+        });
+    });
+});
